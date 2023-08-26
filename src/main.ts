746339bdb703b278053a13ff83d24f768753f83a
Xchen <1322841106@qq.com>
2023-08-26 10:56:59 +0800

    refactor:基础框架搭建

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "@arco-design/web-vue/dist/arco.css";
+import ArcoVue from "@arco-design/web-vue";
+import "@/plugins/axios";
+import "@/access";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).use(ArcoVue).mount("#app");
